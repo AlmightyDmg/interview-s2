8c5f64c455722a6172f657306c0d8bd3653a70e0
zhum <zhumeng@skyvis.com.cn>
2021-01-07 13:56:06 +0800

    Initial commit

---
+++ src/main/java/cn/com/dmg/interviews2/InterviewS2Application.java
@@ -0,0 +1,13 @@
+package cn.com.dmg.interviews2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewS2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewS2Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/dmg/interviews2/InterviewS2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.dmg.interviews2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewS2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
